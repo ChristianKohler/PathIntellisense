55bf246496c89201db8d236aa55fdc8f788ede97
chris <chris@chriss-MacBook.local>
2016-12-11 13:08:00 +0100

    Refactoring towards more testable function

--- src/PathIntellisense.ts
+++ src/PathIntellisense.ts
@@ -1,53 +1,62 @@
-import { CompletionItemProvider, TextDocument, Position, CompletionItem, workspace } from 'vscode';
+import { CompletionItemProvider, TextDocument, Position, CompletionItem, workspace, Range } from 'vscode';
 import { isImportOrRequire, getTextWithinString, importStringRange } from './text-parser';
 import { getPath, extractExtension, Mapping } from './fs-functions';
 import { PathCompletionItem } from './PathCompletionItem';
 import { UpCompletionItem } from './UpCompletionItem';
 import { getConfig, Config } from './config';
 
+interface State {
+    config: Config,
+    fileName: string,
+    textCurrentLine: string,
+    textWithinString: string,
+    importRange: Range,
+    isImport: boolean,
+    documentExtension: string
+}
+
 export class PathIntellisense implements CompletionItemProvider {
     
     constructor(private getChildrenOfPath: Function) { }
     
     provideCompletionItems(document: TextDocument, position: Position): Thenable<CompletionItem[]> {
-        const config = getConfig();
         const textCurrentLine = document.getText(document.lineAt(position).range);
-        const textWithinString = getTextWithinString(textCurrentLine, position.character);
-        const importRange = importStringRange(textCurrentLine, position);
-        const isImport = isImportOrRequire(textCurrentLine);
-        const documentExtension = extractExtension(document);
 
-        if (!this.shouldProvide(textWithinString, isImport, config)) {
-            return Promise.resolve([]);
-        }
+        const state: State = {
+            config: getConfig(),
+            fileName: document.fileName,
+            textCurrentLine,
+            textWithinString: getTextWithinString(textCurrentLine, position.character),
+            importRange: importStringRange(textCurrentLine, position),
+            isImport: isImportOrRequire(textCurrentLine),
+            documentExtension: extractExtension(document)
+        };
         
-        return this.provide(document.fileName, textWithinString, config, isImport, documentExtension, importRange);
+        return this.shouldProvide(state) ? this.provide(state) : Promise.resolve([]);
     }
 
-    shouldProvide(textWithinString: string, isImport: boolean, config: Config) {
-        const typedAnything = textWithinString && textWithinString.length > 0;
-        const startsWithDot = typedAnything && textWithinString[0] === '.';
-        const startsWithMapping = config.mappings.some(mapping => textWithinString.indexOf(mapping.key) === 0);
+    shouldProvide(state: State) {
+        const typedAnything = state.textWithinString && state.textWithinString.length > 0;
+        const startsWithDot = typedAnything && state.textWithinString[0] === '.';
+        const startsWithMapping = state.config.mappings.some(mapping => state.textWithinString.indexOf(mapping.key) === 0);
 
-        if (isImport && (startsWithDot || startsWithMapping)) {
+        if (state.isImport && (startsWithDot || startsWithMapping)) {
             return true;
         }
 
-        if (!isImport && typedAnything) {
+        if (!state.isImport && typedAnything) {
             return true;
         }
 
         return false;
     }
 
-    provide(fileName, textWithinString, config: Config, isImport, documentExtension, importRange) {
-        const path = getPath(fileName, textWithinString, config.mappings);
+    provide(state: State) {
+        const path = getPath(state.fileName, state.textWithinString, state.config.mappings);
         
         return this.getChildrenOfPath(path).then(children => ([
             new UpCompletionItem(),
-            ...children.map(child => new PathCompletionItem(child, importRange, isImport, documentExtension, config))
+            ...children.map(child => new PathCompletionItem(child, state.importRange, state.isImport, state.documentExtension, state.config))
         ]));
     }
-
-    
 }
