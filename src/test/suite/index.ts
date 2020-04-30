509e4ecd656df571c84bfb8c4773a123e2105232
Chris <chris@Chriss-MacBook-Pro.local>
2020-04-30 23:22:15 +0200

    fix(test): set timout to 10s

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -5,7 +5,7 @@ import * as glob from "glob";
 export function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
-    timeout: 5000,
+    timeout: 10000,
     ui: "tdd",
   });
   mocha.useColors(true);
